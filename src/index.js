a34c85c300809d7fba479565968f428a9bf2ca5c
Kevin Millar <koden.km@gmail.com>
2016-12-14 12:51:37 +1000

    First React Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('My SoundCloud React Redux App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const tracks = [
+	{
+		title: 'Some track'
+	},
+	{
+		title: 'Some other track'
+	},
+	{
+		title: 'Some other track 2  two'
+	}
+];
+
+ReactDOM.render(
+	<div>
+		{
+			tracks.map((track, key) => {
+				return <div className="track" key={key}>{track.title}</div>;
+			})
+		}
+	</div>,
+	document.getElementById('app')
+);
