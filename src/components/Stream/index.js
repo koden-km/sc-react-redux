818859275c01b5c917c54b9e8130be582645b6c2
Kevin Millar <koden.km@gmail.com>
2016-12-15 11:38:33 +1000

    Connect Redux and React. Container and Presenter Component

---
+++ src/components/Stream/index.js
@@ -0,0 +1,13 @@
+// Note to self: This is a "container" type component.
+import React from 'react';
+import { connect } from 'react-redux';
+import Stream from './presenter';
+
+function mapStateToProps(state) {
+    const tracks = state.track;
+    return {
+        tracks
+    }
+}
+
+export default connect(mapStateToProps)(Stream);
