5441da957a107f284bc3e0393b6e04b2ea2c3856
Kevin Millar <koden.km@gmail.com>
2016-12-19 15:29:22 +1000

    SoundCloud App. Fetch Tracks

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
 import SC from 'soundcloud';
 import * as actionTypes from '../constants/actionTypes';
+import { setTracks } from '../actions/track';
 
 function setMe(user) {
     return {
@@ -12,14 +13,18 @@ export function auth() {
     // Temp hax until i get an API CLIENT_ID and can use the real function below.
     return function (dispatch) {
         // console.log('Debug: src/actions/auth.js - auth()');
-        dispatch(fetchMe({}));
+        const session = {};
+        dispatch(fetchMe(session));
+        dispatch(fetchStream(session));
     };
 
     // return function (dispatch) {
     //     SC.connect()
-    //     .then((session) => {
-    //         dispatch(fetchMe(session));
-    //     });
+    //         .then((session) => {
+    //             dispatch(fetchMe(session));
+    //             dispatch(fetchStream(session));
+    //         })
+    //     ;
     // };
 };
 
@@ -32,9 +37,41 @@ function fetchMe(session) {
 
     // return function (dispatch) {
     //     fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         dispatch(setMe(data));
-    //     });
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             dispatch(setMe(data));
+    //         })
+    //     ;
+    // };
+}
+
+function fetchStream(session) {
+    // Temp hax until i get an API CLIENT_ID and can use the real function below.
+    return function (dispatch) {
+        // console.log('Debug: src/actions/auth.js - fetchStream()');
+        const data = {
+            collection: [
+                {
+                    origin: {
+                        title: 'Some track'
+                    }
+                },
+                {
+                    origin: {
+                        title: 'Some other track'
+                    }
+                }
+            ]
+        }
+        dispatch(setTracks(data.collection));
+    }
+
+    // return function (dispatch) {
+    //     fetch(`//api.soundcloud.com/me/activities?limit=20&offset=0&oauth_token=${session.oauth_token}`)
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             dispatch(setTracks(data.collection));
+    //         })
+    //     ;
     // };
 }
